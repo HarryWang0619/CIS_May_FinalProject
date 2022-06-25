da7c47210e3a19b0372442ff55d79ccd2dcc41f6
Harry <harrywang0619@gmail.com>
2022-06-25 00:26:54 -0400

    new commit

---
+++ util.py
@@ -0,0 +1,21 @@
+import math
+import csv
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def importfilelist(name:str,delimit:str):
+    # importfile('hw3_cancerdata.csv', '\t')
+    file = open("datasets/"+name, encoding='utf-8-sig')
+    reader = csv.reader(file, delimiter=delimit)
+    dataset = []
+    for row in reader:
+        dataset.append(row)
+    file.close()
+    return dataset
+
+def importdf(name:str, delimit:str):  
+    df = pd.read_csv(name,sep=delimit,header=None)
+    return df
