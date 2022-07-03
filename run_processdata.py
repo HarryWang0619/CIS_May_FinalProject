2f0115c103981b7d392f897eb80fe00e26d8454d
Harry <harrywang0619@gmail.com>
2022-07-02 23:15:20 -0400

    processeddata

---
+++ run_processdata.py
@@ -0,0 +1,30 @@
+import csv
+
+def writehelper(i, writein):
+    filename = 'ProcessedData/pbpb_' + str(i) + '.csv'
+    f = open(filename, 'w')
+    writer = csv.writer(f)
+    writer.writerows(writein)
+    f.close()
+
+
+def readandwrite(): #this read line from PbPb file, and recognize event start and end. return rows
+    with open('Pbpb_2015.txt') as f:
+        eventindex = -1
+        writeinrows = []
+        while True:
+            thisline = f.readline()
+            if "Number" not in thisline and not thisline.__eq__('\n') and not thisline.__eq__(''):
+                writeinrows.append(list(thisline.split()))
+            if "Event" in thisline:
+                eventindex += 1
+                if eventindex != 0:
+                    writehelper(eventindex-1, writeinrows)
+                    writeinrows = []
+            if not thisline:
+                writehelper(eventindex, writeinrows)
+                break
+        # writehelper(eventindex-1, writeinrows)
+    return
+
+readandwrite()
